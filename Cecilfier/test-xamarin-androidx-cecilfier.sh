2932a0fc7d751cf4c99dc62cc0add837d8f25763
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-03-06 13:34:34 +0100

    tests + test script for commandline API

---
+++ Cecilfier/test-xamarin-androidx-cecilfier.sh
@@ -0,0 +1,31 @@
+#/!bin/bash
+
+# test-xamarin-androidx-cecilfier.sh
+
+# sh ./build.sh 
+# dotnet publish ./samples/Xamarin.AndroidX.Cecilfier.App
+# cp ./samples/
+
+
+
+./bin/Xamarin.AndroidX.Cecilfier.App \
+    /exact-pairs \
+        ../Demo/AarxerciseDemoApp/bin/Debug/Aarxercise.dll=../Demo/AarxerciseDemoApp/bin/Debug/Aarxercise.AAA.dll \
+        ../Demo/AarxerciseDemoApp/bin/Debug/ManagedAarxercise.dll:../Demo/AarxerciseDemoApp/bin/Debug/ManagedAarxercise.AAA.dll \
+    /bulk-with-pattern \
+        c1:d1 \
+        c2=d2 \
+
+# BabySteps not building
+#        ../Demo/BabySteps/bin/Debug/BabySteps.dll:../Demo/BabySteps/bin/Debug/BabySteps.AAAA.dll \
+
+
+# mono \
+#     ./samples/Xamarin.AndroidX.Cecilfier.App/bin/Debug/netcoreapp3.0/Xamarin.AndroidX.Cecilfier.App.dll \
+#     /exact-pairs \
+#         ../Demo/BabySteps/bin/Debug/BabySteps.dll:../Demo/BabySteps/bin/Debug/BabySteps.AAAA.dll \
+#         ../Demo/AarxerciseDemoApp/bin/Debug/Aarxercise.dll=../Demo/AarxerciseDemoApp/bin/Debug/Aarxercise.AAA.dll \
+#         ../Demo/AarxerciseDemoApp/bin/Debug/ManagedAarxercise.dll:../Demo/AarxerciseDemoApp/bin/Debug/ManagedAarxercise.AAA.dll \
+#     /bulk-with-pattern \
+#         c1:d1 \
+#         c2=d2 \
