20b447b47807a82406028bd3b998f8a94f6c9287
Matthew Leibowitz <mattleibow@live.com>
2019-03-09 01:39:22 +0200

    A fair overhaul of the type mapping  - using the new mapping file  - can detect when the might be JNI strings

---
+++ Demo/Hackathon/CsvMapping.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Hackathon
+{
+	public class CsvMapping
+	{
+		private readonly SortedDictionary<string, FullType> mapping = new SortedDictionary<string, FullType>();
+		private readonly SortedDictionary<string, FullType> reverseMapping = new SortedDictionary<string, FullType>();
+
+		public CsvMapping(string filename)
+		{
+			Filename = filename ?? throw new ArgumentNullException(nameof(filename));
+			LoadMapping(Filename);
+		}
+
+		public string Filename { get; }
+
+		private void LoadMapping(string path)
+		{
+			mapping.Clear();
+
+			foreach (var line in File.ReadAllText(path).Split('\r', '\n'))
+			{
+				if (string.IsNullOrWhiteSpace(line))
+					continue;
+
+				var split = line.Split(',');
+
+				if (split.Length < (int)Column.Messages)
+					continue;
+
+				var supportNamespace = split[(int)Column.SupportNetNamespace];
+				var supportType = split[(int)Column.SupportNetType];
+				var xNamespace = split[(int)Column.AndroidXNetNamespace];
+				var xType = split[(int)Column.AndroidXNetType];
+				var xAssembly = split[(int)Column.AndroidXNetAssembly];
+
+				if (string.IsNullOrWhiteSpace(supportNamespace) ||
+					string.IsNullOrWhiteSpace(supportType) ||
+					string.IsNullOrWhiteSpace(xNamespace) ||
+					string.IsNullOrWhiteSpace(xType) ||
+					string.IsNullOrWhiteSpace(xAssembly))
+					continue;
+
+				var support = new FullType(supportNamespace, supportType);
+				var androidX = new FullType(xAssembly, xNamespace, xType);
+
+				mapping[support.FullName] = androidX;
+				reverseMapping[androidX.FullName] = support;
+			}
+		}
+
+		public bool TryGetAndroidXType(string supportFullName, out FullType androidxType) =>
+			mapping.TryGetValue(supportFullName, out androidxType);
+
+		public bool TryGetSupportType(string androidxFullName, out FullType supportType) =>
+			reverseMapping.TryGetValue(androidxFullName, out supportType);
+
+		public bool ContainsSupportType(string supportFullName) =>
+			mapping.ContainsKey(supportFullName);
+
+		public bool ContainsAndroidXType(string androidxFullName) =>
+			reverseMapping.ContainsKey(androidxFullName);
+
+		public enum Column
+		{
+			SupportNetNamespace,
+			SupportNetType,
+			AndroidXNetNamespace,
+			AndroidXNetType,
+			AndroidXNetAssembly,
+			SupportJavaPackage,
+			SupportJavaClass,
+			AndroidXJavaPackage,
+			AndroidXJavaClass,
+			Messages
+		}
+	}
+}
--- Demo/Hackathon/Program.cs
+++ Demo/Hackathon/Program.cs
@@ -1,141 +1,19 @@
-﻿using Mono.Cecil;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 
 namespace Hackathon
 {
-	class Program
+	public class Program
 	{
-		private static Dictionary<string, string> assemblyMappings = new Dictionary<string, string>
-		{
-			{ "Xamarin.Android.Support.v7.AppCompat", "Xamarin.AndroidX.AppCompat" },
-			{ "Xamarin.Android.Support.Fragment", "Xamarin.AndroidX.Fragment" },
-			{ "Xamarin.Android.Support.Compat", "Xamarin.AndroidX.Core" },
-			{ "Xamarin.Android.Support.Core.UI", "Xamarin.AndroidX.Legacy.Support.Core.UI" },
-			{ "Xamarin.Android.Support.Design", "Xamarin.Google.Android.Material" },
-			{ "Xamarin.Android.Support.v7.CardView", "Xamarin.AndroidX.CardView" },
-			{ "Xamarin.Android.Support.v7.RecyclerView", "Xamarin.AndroidX.RecyclerView" },
-			{ "Xamarin.Android.Support.DrawerLayout", "Xamarin.AndroidX.DrawerLayout" },
-			{ "Xamarin.Android.Support.ViewPager", "Xamarin.AndroidX.ViewPager" },
-			{ "Xamarin.Android.Support.SwipeRefreshLayout", "Xamarin.AndroidX.SwipeRefreshLayout" },
-			{ "Xamarin.Android.Support.CoordinaterLayout", "Xamarin.AndroidX.CoordinatorLayout" },
-		};
-
 		static void Main(string[] args)
 		{
 			var filename = args.Length > 0 ? args[0] : string.Empty;
 			var migrated = args.Length > 1 ? args[1] : Path.ChangeExtension(filename, "migrated.dll");
 
-			var csv = LoadMapping("mappings.csv");
+			var root = Path.GetDirectoryName(typeof(CsvMapping).Assembly.Location);
+			var path = Path.Combine(root, "androidx-mapping.csv");
 
-			var hasPdb = File.Exists(Path.ChangeExtension(filename, "pdb"));
-
-			var resolver = new DefaultAssemblyResolver();
-			resolver.AddSearchDirectory(Path.GetDirectoryName(filename));
-			var readerParams = new ReaderParameters
-			{
-				ReadSymbols = hasPdb,
-				AssemblyResolver = resolver
-			};
-			var assembly = AssemblyDefinition.ReadAssembly(filename, readerParams);
-			//Console.WriteLine($"Processing assembly '{filename}'...");
-
-			var needsMigration = false;
-			foreach (var module in assembly.Modules)
-			{
-				foreach (var typeRef in module.GetTypeReferences())
-				{
-					if (!csv.TryGetValue(typeRef.FullName, out var newName) || typeRef.FullName == newName.FN)
-						continue;
-
-					Console.WriteLine($" => Processing type reference '{typeRef.FullName}'...");
-
-					var old = typeRef.FullName;
-					typeRef.Namespace = newName.NS;
-					Console.WriteLine($"     => Mapped type '{old}' to '{typeRef.FullName}'.");
-
-					if (assemblyMappings.TryGetValue(typeRef.Scope.Name, out var newAssembly) && typeRef.Scope.Name != newAssembly)
-					{
-						Console.WriteLine($"     => Mapped assembly '{typeRef.Scope.Name}' to '{newAssembly}'.");
-						typeRef.Scope.Name = newAssembly;
-					}
-					else if (assemblyMappings.ContainsValue(typeRef.Scope.Name))
-					{
-						Console.WriteLine($"     => Already mapped assembly '{typeRef.Scope.Name}'.");
-					}
-					else
-					{
-						Console.WriteLine($"     *** Potential error for assembly {typeRef.Scope.Name}' ***");
-					}
-
-					needsMigration = true;
-				}
-			}
-
-			if (needsMigration)
-			{
-				var parameters = new WriterParameters
-				{
-					WriteSymbols = hasPdb
-				};
-
-				assembly.Write(migrated + ".temp", parameters);
-				assembly.Dispose();
-
-				File.Delete(migrated);
-
-				if (hasPdb)
-				{
-					File.Delete(Path.ChangeExtension(migrated, "pdb"));
-					File.Move(migrated + ".temp", migrated);
-					File.Move(migrated + ".pdb", Path.ChangeExtension(migrated, "pdb"));
-				}
-
-				Console.WriteLine($"Migrated assembly to '{migrated}'.");
-			}
-		}
-
-		private static Dictionary<string, (string NS, string T, string FN)> LoadMapping(string csvFile)
-		{
-			var root = Path.GetDirectoryName(typeof(Program).Assembly.Location);
-
-			var dic = new Dictionary<string, (string NS, string T, string FN)>();
-
-			foreach (var line in File.ReadAllText(Path.Combine(root, csvFile)).Split('\r', '\n'))
-			{
-				if (string.IsNullOrWhiteSpace(line))
-					continue;
-
-				var split = line.Split(',');
-
-				var support = split[(int)Columns.AndroidSupportClassFullyQualified];
-				var ns = split[(int)Columns.ManagedNamespaceXamarinAndroidX];
-				var androidx = split[(int)Columns.AndroidXClassFullyQualified];
-
-				if (string.IsNullOrWhiteSpace(support) ||
-					string.IsNullOrWhiteSpace(support) ||
-					string.IsNullOrWhiteSpace(androidx))
-					continue;
-
-				var t = androidx.Substring(ns.Length + 1);
-				dic[support] = (NS: ns, T: t, FN: androidx);
-			}
-
-			return dic;
-		}
-
-		private enum Columns
-		{
-			ClassName,
-			AndroidSupportClass,
-			AndroidXClass,
-			AndroidSupportClassFullyQualified,
-			AndroidXClassFullyQualified,
-			PackageAndroidSupport,
-			PackageAndroidX,
-			ManagedNamespaceXamarinAndroidSupport,
-			ManagedNamespaceXamarinAndroidX
+			var migrator = new Migrator(path);
+			migrator.Migrate(filename, migrated, true);
 		}
 	}
 }
