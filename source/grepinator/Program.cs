d7a08e73bff837114d5dd3886e37a71ec0305207
Matthew Leibowitz <mattleibow@live.com>
2019-03-21 22:27:09 +0200

    Added an app to find Java classes

---
+++ source/grepinator/Program.cs
@@ -0,0 +1,28 @@
+﻿using Mono.Options;
+
+namespace grepinator
+{
+	class Program
+	{
+		public const string Name = "grepinator";
+
+		public static bool Verbose;
+
+		static int Main(string[] args)
+		{
+			var commands = new CommandSet(Name)
+			{
+				$"usage: {Name} COMMAND [OPTIONS]",
+				"",
+				"A utility that helps locate Java classes.",
+				"",
+				"Global options:",
+				{ "v|verbose", "Use a more verbose output", _ => Verbose = true },
+				"",
+				"Available commands:",
+				new SearchCommand(),
+			};
+			return commands.Run(args);
+		}
+	}
+}
