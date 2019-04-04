7ea7a964db3a67f8d13e5d854a222464f7b35d43
Matthew Leibowitz <mattleibow@live.com>
2019-04-04 03:41:40 +0200

    Added a Xamarin.Forms sample

---
+++ samples/FormsDemo/FormsDemo/Views/NewItemPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FormsDemo.Models;
+using Xamarin.Forms;
+
+namespace FormsDemo.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+
+			Item = new Item
+			{
+				Text = "Item name",
+				Description = "This is an item description."
+			};
+
+			BindingContext = this;
+		}
+
+		async void Save_Clicked(object sender, EventArgs e)
+		{
+			MessagingCenter.Send(this, "AddItem", Item);
+			await Navigation.PopModalAsync();
+		}
+
+		async void Cancel_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PopModalAsync();
+		}
+	}
+}
