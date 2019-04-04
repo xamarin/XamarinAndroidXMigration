4c2a507c06c77744b73cfdb69257a8681d6cbd7d
Matthew Leibowitz <mattleibow@live.com>
2019-04-04 19:22:03 +0200

    Tweak a few things

--- samples/BindingsApp/BindingsApp/MainActivity.cs
+++ samples/BindingsApp/BindingsApp/MainActivity.cs
@@ -1,32 +1,50 @@
 ﻿using System;
 using Android.App;
+using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 using Android.Views;
+using Com.Xamarin;
 
 namespace BindingsApp
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
 	public class MainActivity : AppCompatActivity
 	{
+		private Aarxerciser native;
+		private SimpleFragment fragment;
+
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
+			Console.WriteLine($"\n*** typeof(AppCompatActivity) => {typeof(AppCompatActivity)}\n");
+
 			base.OnCreate(savedInstanceState);
+
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
 			SetContentView(Resource.Layout.activity_main);
 
-			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
 			SetSupportActionBar(toolbar);
 
-			FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+			var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
 			fab.Click += FabOnClick;
+
+			native = new Aarxerciser();
+
+			fragment = native.CreateSimpleFragment(this);
+
+			base.SupportFragmentManager.BeginTransaction()
+				.Replace(Resource.Id.frame, fragment)
+				.Commit();
 		}
 
 		public override bool OnCreateOptionsMenu(IMenu menu)
 		{
 			MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+
 			return true;
 		}
 
@@ -34,21 +52,17 @@ namespace BindingsApp
 		{
 			int id = item.ItemId;
 			if (id == Resource.Id.action_settings)
-			{
 				return true;
-			}
 
 			return base.OnOptionsItemSelected(item);
 		}
 
 		private void FabOnClick(object sender, EventArgs eventArgs)
 		{
-			View view = (View)sender;
-			Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-				.SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+			native.UpdateSimpleFragment(fragment, "Java Fragment");
 		}
 
-		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
 		{
 			Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
