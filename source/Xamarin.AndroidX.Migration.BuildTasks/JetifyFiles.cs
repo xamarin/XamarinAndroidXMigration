7e36b1c39a92ddaa6e2245b6ccddf21b4fbc5773
Matthew Leibowitz <mattleibow@live.com>
2019-05-01 21:43:17 +0200

    Adding support for jetifying manifests and proguards

--- source/Xamarin.AndroidX.Migration.BuildTasks/JetifyFiles.cs
+++ source/Xamarin.AndroidX.Migration.BuildTasks/JetifyFiles.cs
@@ -12,21 +12,14 @@ using System.Linq;
 namespace Xamarin.AndroidX.Migration.BuildTasks {
 	public class JetifyFiles : Task {
 
-		#region Class Variables
-
-		readonly string jetifiedSuffix = "-jetified";
-		readonly string [] resourcesName = { "__AndroidLibraryProjects__.zip" };
-		readonly string cachePath = Path.GetTempPath ();
-
-		#endregion
-
-		#region Properties
+		// file inputs
 
 		public ITaskItem [] Files { get; set; }
-		[Output]
 		public ITaskItem [] JetifiedFiles { get; set; }
+		public string JetifiedDirectory { get; set; }
 
-		public string JetifierWrapperPath { get; set; }
+		// configuration inputs
+
 		public string ConfigurationPath { get; set; }
 		public bool Verbose { get; set; }
 		public bool Dejetify { get; set; }
@@ -35,235 +28,76 @@ namespace Xamarin.AndroidX.Migration.BuildTasks {
 		public bool ShouldStripSignatures { get; set; }
 		public bool IsProGuard { get; set; }
 		public bool NoParallel { get; set; }
-		public bool PrintHelp { get; set; }
-		public bool NoOverrideFiles { get; set; }
-
-		#endregion
 
 		public override bool Execute ()
 		{
-			string message;
+			// make sure there is input
 			if (Files == null || Files.Length == 0) {
-				message = $"There's nothing to jetify. Please, set the \"{nameof (Files)}\" attribute.";
-				var exception = new ArgumentNullException (nameof (Files), message);
-				Log.LogErrorFromException (exception);
-				throw exception;
-			}
-
-			if (NoOverrideFiles && Files.Length != JetifiedFiles?.Length) {
-				message = $"The length of {nameof (Files)} and {nameof (JetifiedFiles)} must be the same.";
-				Log.LogError (message);
+				Log.LogError ($"Nothing to jetify. No files were provided via the \"{nameof (Files)}\" attribute.");
 				return false;
 			}
 
-			var archivesToJetify = CreateMigrationPairs (Files, JetifiedFiles);
-
-			var jetifier = new Jetifier {
-				ConfigurationPath = ConfigurationPath,
-				Verbose = Verbose,
-				Dejetify = Dejetify,
-				IsStrict = IsStrict,
-				ShouldRebuildTopOfTree = ShouldRebuildTopOfTree,
-				ShouldStripSignatures = ShouldStripSignatures,
-				IsProGuard = IsProGuard,
-				NoParallel = NoParallel,
-				PrintHelp = PrintHelp
-			};
+			// make sure the output files are valid
+			if (JetifiedFiles?.Length > 0) {
+				if (Files.Length != JetifiedFiles?.Length){
+					Log.LogError ($"The length of {nameof (Files)} and {nameof (JetifiedFiles)} must be the same.");
+					return false;
+				}
+				if (!string.IsNullOrEmpty(JetifiedDirectory)) {
+					Log.LogError ($"The {nameof (JetifiedDirectory)} and {nameof (JetifiedFiles)} cannot both be set.");
+					return false;
+				}
+			}
 
 			try {
-				jetifier.Jetify (archivesToJetify);
+				var filesToJetify = CreateMigrationPairs ().ToList ();
+
+				foreach (var file in filesToJetify)
+					Log.LogMessage ($"Queuing jetification for {file.Source} to {file.Destination}.");
+
+				var jetifier = new Jetifier {
+					ConfigurationPath = ConfigurationPath,
+					Verbose = Verbose,
+					Dejetify = Dejetify,
+					IsStrict = IsStrict,
+					ShouldRebuildTopOfTree = ShouldRebuildTopOfTree,
+					ShouldStripSignatures = ShouldStripSignatures,
+					IsProGuard = IsProGuard,
+					NoParallel = NoParallel,
+				};
+
+				if (!string.IsNullOrEmpty(JetifiedDirectory) && !Directory.Exists (JetifiedDirectory))
+					Directory.CreateDirectory (JetifiedDirectory);
+
+				jetifier.Jetify (filesToJetify);
 			} catch (Exception ex) {
 				Log.LogErrorFromException (ex, true);
 				return false;
 			}
 
-			if (JetifiedFiles == null)
-				JetifiedFiles = archivesToJetify.Select (a => new TaskItem (a.Destination)).ToArray ();
-
 			return true;
 		}
 
-		#region Internal Functionality
-
-		public IEnumerable<MigrationPair> CreateMigrationPairs (ITaskItem [] files, ITaskItem [] jetifiedFiles)
+		private IEnumerable<MigrationPair> CreateMigrationPairs ()
 		{
-			var archivesToJetify = new List<MigrationPair> ();
-			var filesLength = files?.Length ?? 0;
-
+			var filesLength = Files?.Length ?? 0;
 			for (int i = 0; i < filesLength; i++) {
-				var inputFile = files [i].ItemSpec;
-				var outputFile = inputFile;
+				var inputFile = Files [i].ItemSpec;
+				var outputFile = GetOutputFile (i) ?? inputFile;
 
-				if (NoOverrideFiles)
-					outputFile = jetifiedFiles? [i].ItemSpec ?? AddJetifiedSuffix (inputFile);
-
-				archivesToJetify.Add ((inputFile, outputFile));
+				yield return (inputFile, outputFile);
 			}
-
-			return archivesToJetify;
 		}
 
-		// Add the jetify suffix to the path
-		public string AddJetifiedSuffix (string path)
+		private string GetOutputFile (int index)
 		{
-			var jetifiedPath = Path.GetDirectoryName (path);
-			jetifiedPath += Path.DirectorySeparatorChar;
-			jetifiedPath += $"{Path.GetFileNameWithoutExtension (path)}{jetifiedSuffix}";
-			jetifiedPath += Path.GetExtension (path);
-			return jetifiedPath;
+			if (JetifiedFiles?.Length > index)
+				return JetifiedFiles [index].ItemSpec;
+
+			if (!string.IsNullOrEmpty (JetifiedDirectory))
+				return Path.Combine (JetifiedDirectory, Guid.NewGuid ().ToString ());
+
+			return null;
 		}
-
-		public string RemoveJetifiedSuffix (string path)
-		{
-			var unjetifiedPath = Path.GetDirectoryName (path);
-			unjetifiedPath += Path.DirectorySeparatorChar;
-			unjetifiedPath += $"{Path.GetFileNameWithoutExtension (path)}".Replace (jetifiedSuffix, "");
-			unjetifiedPath += Path.GetExtension (path);
-			return unjetifiedPath;
-		}
-
-		public bool CreateJetifiedFile (string filePath)
-		{
-			var unjetifiedPath = RemoveJetifiedSuffix (filePath);
-
-			try {
-				using (var unjetifiedStream = File.Open (unjetifiedPath, FileMode.Open))
-				using (var fileStream = File.Open (filePath, FileMode.Create)) {
-					unjetifiedStream.CopyTo (fileStream);
-				}
-				return true;
-			} catch (Exception ex) {
-				Log.LogError ($"Could not create the jetified {filePath} file.\n{ex.Message}");
-				return false;
-			}
-
-		}
-
-		// Extract the resource to the disk
-		public bool ExtractResourceToDiskIfExists (string dllPath, string resourceName, string resourcePath)
-		{
-			var assembly = AssemblyDefinition.ReadAssembly (dllPath);
-
-			foreach (var resource in assembly.MainModule.Resources) {
-				if (resource.Name.ToLower () != resourceName.ToLower ())
-					continue;
-
-				try {
-					using (var resourceStream = File.Open (resourcePath, FileMode.Create)) {
-						var embeddedResource = (EmbeddedResource)resource;
-						var stream = embeddedResource.GetResourceStream ();
-						stream.CopyTo (resourceStream);
-					}
-					return true;
-				} catch (Exception ex) {
-					Log.LogWarning ($"Could not extract the {resourceName} resource file from {dllPath} and save it into {resourcePath}.\nException message: {ex.Message}");
-					return false;
-				}
-			}
-
-			Log.LogWarning ($"Could not find {resourceName} resource file inside of {dllPath}");
-
-			return false;
-		}
-
-		// Deletes the resource from dll and embed the new one
-		public bool ReplaceResource (string dllPath, string resourcePath)
-		{
-			if (!File.Exists (resourcePath)) {
-				Log.LogError ($"The jetified {resourcePath} resource file to embed into {dllPath} does not exist.");
-				return false;
-			}
-
-			var resourceName = Path.GetFileName (resourcePath);
-			var definition = AssemblyDefinition.ReadAssembly (dllPath);
-
-			for (var i = 0; i < definition.MainModule.Resources.Count; i++) {
-				if (definition.MainModule.Resources [i].Name != resourceName)
-					continue;
-
-				definition.MainModule.Resources.RemoveAt (i);
-				break;
-			}
-
-			try {
-				using (var resourceStream = File.Open (resourcePath, FileMode.Open)) {
-					var embeddedResource = new EmbeddedResource (resourceName, ManifestResourceAttributes.Public, resourceStream);
-					definition.MainModule.Resources.Add (embeddedResource);
-					definition.Write (dllPath);
-				}
-				return true;
-			} catch (Exception ex) {
-				Log.LogError ($"Could not embed the jetified {resourcePath} resource file into {dllPath}.\n{ex.Message}");
-				return false;
-			}
-		}
-
-		#endregion
-
-		//public override bool Execute ()
-		//{
-		//	var archivesToJetify = new List<MigrationPair> ();
-		//	var archivesToEmbed = new List<ArchivesPair> ();
-
-		//	for (int i = 0; i < Files.Length; i++) {
-		//		var inputFile = Files [i].ItemSpec;
-		//		var outputFile = inputFile;
-
-		//		// If .dll, we need to extract the zip file and jetify it.
-		//		if (inputFile.EndsWith (".dll", true, CultureInfo.CurrentCulture)) {
-		//			var dllFile = inputFile;
-		//			var resources = new List<string> ();
-
-		//			// Extract the zip file and add it to the list to jetify
-		//			foreach (var resourceName in resourcesName) {
-		//				inputFile = Path.Combine (cachePath, Path.GetFileNameWithoutExtension (dllFile), resourceName);
-		//				outputFile = inputFile;
-
-		//				if (!ExtractResourceToDiskIfExists (dllFile, resourceName, inputFile))
-		//					continue;
-
-		//				if (NoOverrideFiles)
-		//					dllFile = AddJetifiedSufix (dllFile);
-
-		//				archivesToJetify.Add ((inputFile, outputFile));
-		//				resources.Add (outputFile);
-		//			}
-
-		//			if (resources.Count > 0)
-		//				archivesToEmbed.Add ((dllFile, resources));
-		//		} else {
-		//			if (NoOverrideFiles)
-		//				outputFile = AddJetifiedSufix (inputFile);
-
-		//			archivesToJetify.Add ((inputFile, outputFile));
-		//		}
-		//	}
-
-		//	var jetifier = new Jetifier {
-		//		ConfigurationPath = ConfigurationPath,
-		//		Verbose = Verbosity,
-		//		Dejetify = Dejetify,
-		//		IsStrict = IsStrict,
-		//		ShouldRebuildTopOfTree = ShouldRebuildTopOfTree,
-		//		ShouldStripSignatures = ShouldStripSignatures,
-		//		NoParallel = NoParallel,
-		//		PrintHelp = PrintHelp
-		//	};
-
-		//	try {
-		//		jetifier.Jetify (archivesToJetify);
-		//	} catch (Exception ex) {
-		//		Log.LogErrorFromException (ex, true);
-		//		return false;
-		//	}
-
-		//	foreach (var archives in archivesToEmbed) {
-		//		foreach (var archive in archives.Archives)
-		//			if (!ReplaceResource (archives.Source, archive))
-		//				return false;
-		//	}
-
-		//	return true;
-		//}
 	}
 }
