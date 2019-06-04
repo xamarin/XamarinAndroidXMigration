fed94c40929185c1f8f91f0f56cfdc23e7a08d4f
Matthew Leibowitz <mattleibow@live.com>
2019-06-04 07:52:07 +0200

    Added a Xamarin.Forms Shell sample app

---
+++ samples/FormsShellApp/FormsShellApp/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FormsShellApp
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ samples/FormsShellApp/FormsShellApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FormsShellApp.Models;
+
+namespace FormsShellApp.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
